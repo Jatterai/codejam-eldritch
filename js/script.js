75e9a5b4f4a220f40612b0feee418b173c45a2e5
Jatterai <maiorova.yuna@gmail.com>
2022-08-25 21:15:54 +0300

    feat: html and css for choosing level done

--- js/script.js
+++ js/script.js
@@ -1,11 +1,50 @@
 
-let antients = document.querySelector('.antients__row');
+let chosenAntient, chosenLevel;
 
-antients.addEventListener('click', (e) => {
-	let targ = e.target.closest('.antients__item');
-	if (!targ) return;
-	let selected = antients.querySelector('.selected');
-	if (selected) selected.classList.remove('selected')
+const antientsList = document.querySelector('.antients__row');
+const antientsButton = document.getElementById('anitents-submit');
+const levelList = document.querySelector('.level__list');
+const startButton = document.querySelector('.level__button')
 
-	targ.classList.add('selected')
+
+function chooser(e) {
+	const li = e.target.closest('li');
+	if (!li) return;
+
+	const checked = e.currentTarget.querySelector('.checked');
+	if (checked) checked.classList.remove('checked');
+	li.classList.add('checked');
+}
+
+[antientsList, levelList].forEach(
+	i => i.addEventListener('click', chooser)
+);
+
+function start(e) {
+	const button = e.target.closest('button.button');
+	if (!button) return;
+	[chosenAntient, chosenLevel] = [document.querySelector('.antient.checked').getAttribute('id'), document.querySelector('.level__item.checked').getAttribute('id')]
+}
+
+function fadeOut(e) {
+	const button = e.target.closest('button.button');
+	if (!button) return;
+	const current = button.closest('section');
+	const next = current.nextElementSibling;
+	current.classList.add('off');
+	setTimeout(() => {
+		current.hidden = true;
+
+		if (next && next.className.includes('off') && next.tagName === 'SECTION') {
+			next.hidden = '';
+			next.classList.remove('off');
+		};
+	}, 700);
+}
+
+
+[antientsButton, startButton].forEach(i => {
+	i.addEventListener('click', fadeOut)
 })
+
+startButton.addEventListener('click', start)
