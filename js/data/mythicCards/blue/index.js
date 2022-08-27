67e6bc1c3a81b645d1d03f120f58109003fda9b9
Jatterai <maiorova.yuna@gmail.com>
2022-08-27 17:32:24 +0300

    fix: some more pathes i am so tired

--- js/data/mythicCards/blue/index.js
+++ js/data/mythicCards/blue/index.js
@@ -1,73 +1,73 @@
 const cardsData = [
 	{
 		id: 'blue1',
-		cardFace: '../../../../img/MythicCards/blue/blue1.png',
+		cardFace: './img/MythicCards/blue/blue1.png',
 		difficulty: 'hard',
 		color: 'blue'
 	},
 	{
 		id: 'blue2',
-		cardFace: '../../../../img/MythicCards/blue/blue2.png',
+		cardFace: './img/MythicCards/blue/blue2.png',
 		difficulty: 'hard',
 		color: 'blue'
 	},
 	{
 		id: 'blue3',
-		cardFace: '../../../../img/MythicCards/blue/blue3.png',
+		cardFace: './img/MythicCards/blue/blue3.png',
 		difficulty: 'easy',
 		color: 'blue'
 	},
 	{
 		id: 'blue4',
-		cardFace: '../../../../img/MythicCards/blue/blue4.png',
+		cardFace: './img/MythicCards/blue/blue4.png',
 		difficulty: 'easy',
 		color: 'blue'
 	},
 	{
 		id: 'blue5',
-		cardFace: '../../../../img/MythicCards/blue/blue5.png',
+		cardFace: './img/MythicCards/blue/blue5.png',
 		difficulty: 'easy',
 		color: 'blue'
 	},
 	{
 		id: 'blue6',
-		cardFace: '../../../../img/MythicCards/blue/blue6.png',
+		cardFace: './img/MythicCards/blue/blue6.png',
 		difficulty: 'hard',
 		color: 'blue'
 	},
 	{
 		id: 'blue7',
-		cardFace: '../../../../img/MythicCards/blue/blue7.png',
+		cardFace: './img/MythicCards/blue/blue7.png',
 		difficulty: 'normal',
 		color: 'blue'
 	},
 	{
 		id: 'blue8',
-		cardFace: '../../../../img/MythicCards/blue/blue8.png',
+		cardFace: './img/MythicCards/blue/blue8.png',
 		difficulty: 'hard',
 		color: 'blue'
 	},
 	{
 		id: 'blue9',
-		cardFace: '../../../../img/MythicCards/blue/blue9.png',
+		cardFace: './img/MythicCards/blue/blue9.png',
 		difficulty: 'normal',
 		color: 'blue'
 	},
 	{
 		id: 'blue10',
-		cardFace: '../../../../img/MythicCards/blue/blue10.png',
+		cardFace: './img/MythicCards/blue/blue10.png',
 		difficulty: 'easy',
 		color: 'blue'
 	},
 	{
 		id: 'blue11',
-		cardFace: '../../../../img/MythicCards/blue/blue11.png',
+		cardFace: './img/MythicCards/blue/blue11.png',
 		difficulty: 'normal',
 		color: 'blue'
 	},
 	{
 		id: 'blue12',
-		cardFace: '../../../../img/MythicCards/blue/blue12.png',
+		cardFace: './img/MythicCards/blue/blue12.png',
 		difficulty: 'normal',
 		color: 'blue'
 	},
--- js/data/mythicCards/brown/index.js
+++ js/data/mythicCards/brown/index.js
@@ -2,127 +2,127 @@
 const cardsData = [
 	{
 		id: 'brown1',
-		cardFace: '../../../../img/MythicCards/brown/brown1.png',
+		cardFace: './img/MythicCards/brown/brown1.png',
 		difficulty: 'normal',
 		color: 'brown'
 	},
 	{
 		id: 'brown2',
-		cardFace: '../../../../img/MythicCards/brown/brown2.png',
+		cardFace: './img/MythicCards/brown/brown2.png',
 		difficulty: 'normal',
 		color: 'brown'
 	},
 	{
 		id: 'brown3',
-		cardFace: '../../../../img/MythicCards/brown/brown3.png',
+		cardFace: './img/MythicCards/brown/brown3.png',
 		difficulty: 'normal',
 		color: 'brown'
 	},
 	{
 		id: 'brown4',
-		cardFace: '../../../../img/MythicCards/brown/brown4.png',
+		cardFace: './img/MythicCards/brown/brown4.png',
 		difficulty: 'normal',
 		color: 'brown'
 	},
 	{
 		id: 'brown5',
-		cardFace: '../../../../img/MythicCards/brown/brown5.png',
+		cardFace: './img/MythicCards/brown/brown5.png',
 		difficulty: 'normal',
 		color: 'brown'
 	},
 	{
 		id: 'brown6',
-		cardFace: '../../../../img/MythicCards/brown/brown6.png',
+		cardFace: './img/MythicCards/brown/brown6.png',
 		difficulty: 'hard',
 		color: 'brown'
 	},
 	{
 		id: 'brown7',
-		cardFace: '../../../../img/MythicCards/brown/brown7.png',
+		cardFace: './img/MythicCards/brown/brown7.png',
 		difficulty: 'hard',
 		color: 'brown'
 	},
 	{
 		id: 'brown8',
-		cardFace: '../../../../img/MythicCards/brown/brown8.png',
+		cardFace: './img/MythicCards/brown/brown8.png',
 		difficulty: 'hard',
 		color: 'brown'
 	},
 	{
 		id: 'brown9',
-		cardFace: '../../../../img/MythicCards/brown/brown9.png',
+		cardFace: './img/MythicCards/brown/brown9.png',
 		difficulty: 'hard',
 		color: 'brown'
 	},
 	{
 		id: 'brown10',
-		cardFace: '../../../../img/MythicCards/brown/brown10.png',
+		cardFace: './img/MythicCards/brown/brown10.png',
 		difficulty: 'hard',
 		color: 'brown'
 	},
 	{
 		id: 'brown11',
-		cardFace: '../../../../img/MythicCards/brown/brown11.png',
+		cardFace: './img/MythicCards/brown/brown11.png',
 		difficulty: 'easy',
 		color: 'brown'
 	},
 	{
 		id: 'brown12',
-		cardFace: '../../../../img/MythicCards/brown/brown12.png',
+		cardFace: './img/MythicCards/brown/brown12.png',
 		difficulty: 'easy',
 		color: 'brown'
 	},
 	{
 		id: 'brown13',
-		cardFace: '../../../../img/MythicCards/brown/brown13.png',
+		cardFace: './img/MythicCards/brown/brown13.png',
 		difficulty: 'easy',
 		color: 'brown'
 	},
 	{
 		id: 'brown14',
-		cardFace: '../../../../img/MythicCards/brown/brown14.png',
+		cardFace: './img/MythicCards/brown/brown14.png',
 		difficulty: 'easy',
 		color: 'brown'
 	},
 	{
 		id: 'brown15',
-		cardFace: '../../../../img/MythicCards/brown/brown15.png',
+		cardFace: './img/MythicCards/brown/brown15.png',
 		difficulty: 'normal',
 		color: 'brown'
 	},
 	{
 		id: 'brown16',
-		cardFace: '../../../../img/MythicCards/brown/brown16.png',
+		cardFace: './img/MythicCards/brown/brown16.png',
 		difficulty: 'normal',
 		color: 'brown'
 	},
 	{
 		id: 'brown17',
-		cardFace: '../../../../img/MythicCards/brown/brown17.png',
+		cardFace: './img/MythicCards/brown/brown17.png',
 		difficulty: 'normal',
 		color: 'brown'
 	},
 	{
 		id: 'brown18',
-		cardFace: '../../../../img/MythicCards/brown/brown18.png',
+		cardFace: './img/MythicCards/brown/brown18.png',
 		difficulty: 'normal',
 		color: 'brown'
 	},
 	{
 		id: 'brown19',
-		cardFace: '../../../../img/MythicCards/brown/brown19.png',
+		cardFace: './img/MythicCards/brown/brown19.png',
 		difficulty: 'normal',
 		color: 'brown'
 	},
 	{
 		id: 'brown20',
-		cardFace: '../../../../img/MythicCards/brown/brown20.png',
+		cardFace: './img/MythicCards/brown/brown20.png',
 		difficulty: 'normal',
 		color: 'brown'
 	},
 	{
 		id: 'brown21',
-		cardFace: '../../../../img/MythicCards/brown/brown21.png',
+		cardFace: './img/MythicCards/brown/brown21.png',
 		difficulty: 'easy',
 		color: 'brown'
 	},
